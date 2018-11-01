73444cdec641ab00e2d8458dcb4b3dce2b848e05
bolixinyu <2410781627@qq.com>
2018-11-01 20:41:27 +0800

    Add files via upload

---
+++ src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="registe-input">
+    <el-row :gutter="20">
+        <el-col :span="24">注册邮箱<el-input v-model="input" placeholder="请输入邮箱地址"></el-input></el-col>
+    </el-row>
+    <el-row :gutter="20">
+        <el-col :span="24">用户名<el-input v-model="input" placeholder="请输入内容"></el-input></el-col>
+    </el-row>
+    <el-row :gutter="20">
+        <el-col :span="24">密码<el-input v-model="input" placeholder="请输入密码"></el-input></el-col>
+    </el-row>
+    <el-row :gutter="20">
+        <el-col :span="24">确认密码<el-input v-model="input" placeholder="请再次输入密码"></el-input></el-col>
+    </el-row>
+    <el-row :gutter="20">
+        <el-button round align="center">圆角按钮</el-button>
+    </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Register",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
