ae15ec66de728e91a3e14b82f864aee4e7443a36
shlinyyb <shlinyyb@gmail.com>
2018-11-28 11:38:23 +0800

    添加路由进行测试

---
+++ src/components/Login_beta1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-input v-model="input_usr"  id="input_usr" placeholder="请输入账号"></el-input>
+    <el-input v-model="input_pwd"  id="input_pwd" placeholder="请输入密码"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            input_usr:'',
+            input_pwd:''
+        }
+    }
+}
+</script>
+
+<style>
+  #input_usr{
+      text-align: center;
+      height: 2.7em;
+      width: 50%;
+  }
+  #input_pwd{
+      align-self: center;
+      width: 50%;
+  }
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Login_beta1 from '@/components/Login_beta1'
 import Home from '@/components/Home'
 import System from '@/components/System'
 import Register from'@/components/Register'
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/Login_beta1',
+      name: 'Login_beta1',
+      component: Login_beta1
     },
     {
       path:'/System',
